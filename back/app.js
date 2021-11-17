28dfbe076e0fe1a23d75609543685f5ee17b99b9
Cynthia Colin <cynthiacolin@hotmail.fr>
2021-11-17 17:06:16 +0100

    auth

--- back/app.js
+++ back/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
 const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
 const app = express();
 mongoose
   .connect(
@@ -27,5 +30,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../middleware/auth');
 const sauceCtrl = require("../controllers/sauces");
 
-router.post("/", sauceCtrl.createThing);
-router.get("/:id", sauceCtrl.getOneThing);
-router.get("/", sauceCtrl.getAllThing);
-router.put("/:id", sauceCtrl.modifyThing);
-router.delete("/:id", sauceCtrl.deleteThing);
+router.post("/", auth, sauceCtrl.createThing);
+router.get("/:id", auth, sauceCtrl.getOneThing);
+router.get("/", auth, sauceCtrl.getAllThing);
+router.put("/:id", auth, sauceCtrl.modifyThing);
+router.delete("/:id", auth, sauceCtrl.deleteThing);
 
 module.exports = router;
