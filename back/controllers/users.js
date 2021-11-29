bba1d365626ed69e79d889cfaba60b3c59a13cce
Cynthia Colin <cynthiacolin@hotmail.fr>
2021-11-29 21:02:50 +0100

    password schema

--- back/controllers/users.js
+++ back/controllers/users.js
@@ -1,69 +1,64 @@
 //Déclaration des constantes et packages à utiliser
-const bcrypt = require('bcrypt');
-const User = require('../models/users');
-const jwt = require('jsonwebtoken');
-const passwordValidator = require('password-validator'); 
-const emailValidator = require("email-validator"); 
-const passwordSchema = new passwordValidator();
+const bcrypt = require("bcrypt");
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
+const passwordSchema = require("../utils/passwordValidator");
+const emailValidator = require("email-validator");
 
-//Modèle du format de mot de passe requis
-passwordSchema
-.is().min(8)                                    
-.is().max(100)                                  
-.has().uppercase()                              
-.has().lowercase()                              
-.has().digits()                                
-.has().not().spaces()   
 
 //Inscription de l'utilisateur
 exports.signup = (req, res, next) => {
   //Vérification du format de l'adresse mail
-  if (!emailValidator.validate(req.body.email)){
-    return res.status(401).json({message: 'Veuillez entrer une adresse email valide'});
-}
-//Vérification du format du mot de passe
-if (!passwordSchema.validate(req.body.password)){ 
-    return res.status(401).json({message: `Le mot de passe doit contenir au moins 8 caractères avec un chiffre, une minuscule, une majuscule et pas d'espace`});
-};
+  if (!emailValidator.validate(req.body.email)) {
+    return res
+      .status(401)
+      .json({ message: "Veuillez entrer une adresse email valide" });
+  }
+  //Vérification du format du mot de passe
+  if (!passwordSchema.validate(req.body.password)) {
+    return res
+      .status(401)
+      .json({
+        message: `Le mot de passe doit contenir au moins 8 caractères avec un chiffre, une minuscule, une majuscule et pas d'espace`,
+      });
+  }
   //utilisation de bcrypt pour hasher le mot de passe
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
       const user = new User({
         email: req.body.email,
-        password: hash
+        password: hash,
       });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
     })
-    .catch(error => res.status(500).json({ message : error.message }));
-  
+    .catch((error) => res.status(500).json({ message: error.message }));
 };
-
 
 //Connexion d'un utilisateur et attribution d'un token utilisateur
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-    .then(user => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
       }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
           if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign(
-                { userId: user._id },
-                'RANDOM_TOKEN_SECRET',
-                { expiresIn: '24h' }
-              )
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
           });
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch((error) => res.status(500).json({ error }));
     })
-    .catch(error => res.status(500).json({ error }));
-    
+    .catch((error) => res.status(500).json({ error }));
 };
