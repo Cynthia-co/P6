fa73fc355dc88a6ce4d284cd13c2c051047d2476
Cynthia Colin <cynthiacolin@hotmail.fr>
2021-11-29 20:46:52 +0100

    finalisation

--- back/models/users.js
+++ back/models/users.js
@@ -1,7 +1,5 @@
 const mongoose = require ('mongoose');
-
 const uniqueValidator = require('mongoose-unique-validator');
-
 
 //Modèle de l'utilisateur pour l'inscription et l'identification
 const userSchema = mongoose.Schema({
